c571eb8c6acf9801754042d224f2e2ecbd0bb305
Edward Timmer <edtimmer@gmail.com>
2020-04-04 19:17:38 -0400

    added

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -10,9 +12,6 @@ class StreamCreate extends React.Component {
         </div>
       );
     }
-    // else {
-    //   return null;
-    // }
   }
 
   renderInput = ({ input, label, meta }) => {
@@ -26,8 +25,8 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = formValues => {
+    this.props.createStream(formValues)
   }
 
   render() {
@@ -61,7 +60,9 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
